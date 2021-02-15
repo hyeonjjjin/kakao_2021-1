611f23aa40e36d752b20b8b1bcab6338eaa1bfc6
hjjja <jahj30734@gmail.com>
2021-02-15 23:42:53 +0900

    clear_testbench

--- src/Solve.java
+++ src/Solve.java
@@ -2,13 +2,13 @@ import java.lang.*;
 
 class Solve {
     public static void main(String[] args) {
-        solution("012345678901234567890123");
+        solution("=.=");
     }
     public static String solution(String new_id) {
         String[] delete = {"~","`","!","@","#","\\$","%","\\^","&","\\*","\\(","\\)","\\+","=","\\|","\\\\","\\{","\\}","\\[","\\]",":",";", "\"","'","<",">","\\?","/"};
         System.out.println(new_id);
         //1단계
-        new_id.toLowerCase();
+        new_id= new_id.toLowerCase();
         System.out.println("1단계 해결"+new_id);
         //2단계
         for (String item : delete) new_id=new_id.replaceAll(item,"");
@@ -17,7 +17,8 @@ class Solve {
         StringBuffer temp_id= new StringBuffer(new_id+' ');
         int dot_start = temp_id.indexOf("..");
         int dot_end = dot_start+1;
-        while(dot_start >0){
+        System.out.println(dot_start+" "+dot_end);
+        while(dot_start >=0){
             while(temp_id.charAt(dot_end+1)=='.') {dot_end++;}
             temp_id=temp_id.delete(dot_start, dot_end);
             dot_start = temp_id.indexOf("..");
@@ -32,16 +33,23 @@ class Solve {
         System.out.println("4단계 해결"+temp_id);
 
         //5단계 **앞서 맨 뒤에 ' '을 추가했음을 유의
-        if(temp_id.length()==1) temp_id.append('a');
+        if(temp_id.length()==1) temp_id.insert(0,'a');
         System.out.println("5단계 해결"+temp_id);
 
         //6단계 **앞서 맨 뒤에 ' '을 추가했음을 유의
         if(temp_id.length()>=17) temp_id=temp_id.delete(15, temp_id.length()-1);
+        if(temp_id.length()>=2&& temp_id.charAt(temp_id.length()-2)=='.') temp_id=temp_id.deleteCharAt(temp_id.length()-2);
         System.out.println("6단계 해결"+temp_id);
+        //System.out.println(temp_id.append('a'));
 
-        //
-        System.out.println(new_id);
-        String answer = "";
-        return answer;
+        //7단계
+        System.out.println("len: "+temp_id.length()+" "+temp_id.charAt(temp_id.length()-2));
+        while(temp_id.length()<4) temp_id.insert(temp_id.length()-1,temp_id.charAt(temp_id.length()-2));
+        System.out.println("7단계 해결"+temp_id);
+
+        return (temp_id.deleteCharAt(temp_id.length()-1)).toString();
+        //System.out.println(new_id);
+        //String answer = "";
+        //return answer;
     }
 }
